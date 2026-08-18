51a63199adfc7f2b1161a00dc30332e2be9808f7
Nikhil kumar <nikhil821023@gmail.com>
2026-08-18 10:01:48 +0530

    Add method to find team by tournament and captain email

--- src/main/java/com/auctionx/repository/TeamRepository.java
+++ src/main/java/com/auctionx/repository/TeamRepository.java
@@ -3,8 +3,10 @@ package com.auctionx.repository;
 import com.auctionx.model.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
+import java.util.Optional;
 
 public interface TeamRepository extends JpaRepository<Team, Long> {
     List<Team> findByTournamentId(Long tournamentId);
     boolean existsByTeamNameAndTournamentId(String teamName, Long tournamentId);
+    Optional<Team> findByTournamentIdAndCaptainEmail(Long tournamentId, String captainEmail);
 }
